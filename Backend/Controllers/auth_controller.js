287ec3fdbdd6c4aa26ee2b8a38fc3c10fb676ed3
SunnyShekhar-code <sunnyshekhar1729@gmail.com>
2025-07-22 12:48:49 +0530

    Update auth_controller.js

--- Backend/Controllers/auth_controller.js
+++ Backend/Controllers/auth_controller.js
@@ -21,8 +21,8 @@ export const signup = async(req,res)=>{
     res.cookie("token",token,{
         httpOnly:true,
         maxAge: 4*24*60*60*1000,
-        sameSite:"Strict",
-        secure:false
+        sameSite:"None",
+        secure:true
 
     });
 
@@ -48,8 +48,8 @@ export const login = async(req,res)=>{
     res.cookie("token",token,{
         httpOnly:true,
         maxAge: 4*24*60*60*1000,
-        sameSite:"Strict",
-        secure:false
+        sameSite:"None",
+        secure:true
 
     });
 
